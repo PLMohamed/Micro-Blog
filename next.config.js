f6dd397a5fbd9768be73bb40d952c26850ddd9d3
Boumedine Mohamed Touati <141277277+PLMohamed@users.noreply.github.com>
2024-02-01 15:46:50 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: "",
-    assetPrefix: "",
-    output: "export",
+  output: 'export',
 };
 
 module.exports = nextConfig
